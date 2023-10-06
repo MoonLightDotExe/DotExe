ae9f3f282036d0c0d597a855b0daed4e16da8734
RushirajChaudhari <rushirajchaudhari657@gmail.com>
2023-10-06 19:53:18 +0530

    Map integration commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Map from './pages/map';
 
 function App() {
-  return <></>
+  return(<>
+  <Router>
+    <Routes>
+      <Route path="/map" element={<Map/>} />
+     </Routes>
+  </Router>
+    </>)
 }
 
 export default App
