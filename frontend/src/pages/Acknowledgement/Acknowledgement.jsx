7b65e865aea9bab07775f06f0509c09605da74b1
Om Amonkar <ompamonkar@gmail.com>
2023-10-07 01:00:03 +0530

    Acknowledgement Page Added

--- frontend/src/pages/Acknowledgement/Acknowledgement.jsx
+++ frontend/src/pages/Acknowledgement/Acknowledgement.jsx
@@ -2,17 +2,45 @@ import React from 'react'
 import './Acknowledgement.css'
 import HCard from '../../components/Card/HCard'
 
+const data = [
+  {
+    head: 'HEAD',
+    addr: 'Flat no. 4, Devidayal Road, Near Raj Hospital, Mulund West',
+    img: 'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2373&q=80',
+  },
+  {
+    head: 'HEAD',
+    addr: 'ADDR',
+    img: 'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2373&q=80',
+  },
+  {
+    head: 'HEAD',
+    addr: 'ADDR',
+    img: 'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2373&q=80',
+  },
+]
+
 function Acknowledgement() {
   return (
-    <div className='card2'>
-      <HCard
-        head='A very Good hospital'
-        addr='C/701,Pleasant Park, Santoshi Mata Road,Dahisar West'
-        img={
-          'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2373&q=80'
-        }
-      />
-    </div>
+    <>
+      <div className='ack_head'>Hang On Tight!</div>
+      <div className='ack_head_desc'>
+        Meanwhile please note the services nearest to you
+      </div>
+      <div className='ack_list'>
+        {data.map((item) => {
+          return (
+            <div className='card2'>
+              <HCard
+                head={item.head}
+                addr={item.addr}
+                img={item.img}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import "./Admin.css";
-import { Chart } from "react-google-charts";
+import React from 'react'
+import './Admin.css'
 import {
-    Stat,
-    StatLabel,
-    StatNumber,
-    StatHelpText,
-    StatArrow,
-    StatGroup,
-    Button, 
-    ButtonGroup
-  } from '@chakra-ui/react'
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  Button,
+} from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
 
 //charts
@@ -23,13 +21,14 @@ import { useToast } from '@chakra-ui/react'
 //     ["Watch TV", 2],
 //     ["Sleep", 7],
 //   ];
-  
+
 //   export const options = {
 //     title: "My Daily Activities",
 //   };
 
-//put in return 
-{/* <div className='chart'>
+//put in return
+{
+  /* <div className='chart'>
     <Chart
       chartType="PieChart"
       data={data}
@@ -37,74 +36,77 @@ import { useToast } from '@chakra-ui/react'
       width={"100%"}
       height={"400px"}
     />
-    </div> */}
+    </div> */
+}
 
 function Admin() {
-    const toast = useToast()
-    setTimeout(() => { toast({
-        position: 'top',
-        title: 'Account created.',
-        description: "We've created your account for you.",
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-        containerStyle: {
-            width: '800px',
-            maxWidth: '100%',
-          },
-        })},2000)
+  const toast = useToast()
+  setTimeout(() => {
+    toast({
+      position: 'top',
+      title: 'Account created.',
+      description: "We've created your account for you.",
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+      containerStyle: {
+        width: '800px',
+        maxWidth: '100%',
+      },
+    })
+  }, 2000)
 
   return (
-<div>
-    <div className='navbar'>
-       
-    </div>
-    <div className='fabricate'>
+    <div>
+      <div className='navbar'></div>
+      <div className='fabricate'>
         <StatGroup>
-            <Stat>
-                <StatLabel>Available Hospital Beds</StatLabel>
-                <StatNumber>30,670</StatNumber>
-                <StatHelpText>
-                <StatArrow type='increase' />
-                23.36%
-                </StatHelpText>
-            </Stat>
+          <Stat>
+            <StatLabel>Available Hospital Beds</StatLabel>
+            <StatNumber>30,670</StatNumber>
+            <StatHelpText>
+              <StatArrow type='increase' />
+              23.36%
+            </StatHelpText>
+          </Stat>
 
-            <Stat>
-                <StatLabel>Available Nurses</StatLabel>
-                <StatNumber>5,900</StatNumber>
-                <StatHelpText>
-                <StatArrow type='decrease' />
-                30.08%
-                </StatHelpText>
-            </Stat>
+          <Stat>
+            <StatLabel>Available Nurses</StatLabel>
+            <StatNumber>5,900</StatNumber>
+            <StatHelpText>
+              <StatArrow type='decrease' />
+              30.08%
+            </StatHelpText>
+          </Stat>
 
-            <Stat>
-                <StatLabel>Available Fireman on standby</StatLabel>
-                <StatNumber>480</StatNumber>
-                <StatHelpText>
-                <StatArrow type='decrease' />
-                9.05%
-                </StatHelpText>
-            </Stat>
+          <Stat>
+            <StatLabel>Available Fireman on standby</StatLabel>
+            <StatNumber>480</StatNumber>
+            <StatHelpText>
+              <StatArrow type='decrease' />
+              9.05%
+            </StatHelpText>
+          </Stat>
 
-            <Stat>
-                <StatLabel>Available Policeman on standby</StatLabel>
-                <StatNumber>10,070</StatNumber>
-                <StatHelpText>
-                <StatArrow type='increase' />
-                18.39%
-                </StatHelpText>
-            </Stat>
+          <Stat>
+            <StatLabel>Available Policeman on standby</StatLabel>
+            <StatNumber>10,070</StatNumber>
+            <StatHelpText>
+              <StatArrow type='increase' />
+              18.39%
+            </StatHelpText>
+          </Stat>
         </StatGroup>
-    </div>
-    <div className='center'>
-        <img src="https://itjunkies.in/assets/images/blogs/map.webp" alt=""/>
-    </div>
+      </div>
+      <div className='center'>
+        <img
+          src='https://itjunkies.in/assets/images/blogs/map.webp'
+          alt=''
+        />
+      </div>
 
-    <Button colorScheme='blue'>Button</Button>
-    
-</div>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
   )
 }
 
