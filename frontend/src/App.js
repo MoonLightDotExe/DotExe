6a903af10e3a26a30f1124c85778416857eca649
RushirajChaudhari <rushirajchaudhari657@gmail.com>
2023-10-07 06:11:49 +0530

    rushi 2nd commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom'
 import Admin from './pages/Admin/Admin'
 import { useLoadScript } from '@react-google-maps/api'
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Acknowledgement from './pages/Acknowledgement/Acknowledgement'
 import MapComp from './pages/MapComp'
-import Home from './pages/Home'
+import Login from './pages/Login/Login';
+import Register from './pages/Login/Register';
 
 function App() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCkIdp1ZbRPtNQ0vZuJgpx8pdlmTrKWts4', // Add your API key
   })
+
+
+
   return (
     <>
       <Router>
@@ -31,9 +35,16 @@ function App() {
             element={<MapComp />}
           />
           <Route
-            path='/home'
-            element={<Home/>}
+            path='/login'
+            element={<Login/>}
           />
+          <Route
+            path='/register'
+            element={<Register/>}
+          />
+          
+
+
         </Routes>
       </Router>
     </>
---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,76 @@
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
+
+export default function HookForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2))
+        resolve()
+      }, 3000)
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.name}>
+        <FormLabel htmlFor='name'>Login Form </FormLabel>
+        <Input
+          id='name'
+          placeholder='name'
+          {...register('name', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        /><br/><br/>
+
+        <Input
+          id='Email'
+          placeholder='email'
+          {...register('email', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        /> <br/><br/>
+
+<Input
+          id='password'
+          placeholder='password'
+          {...register('password', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        /><br/><br/>
+<Input
+          id='Confirm Password'
+          placeholder='confirm-password'
+          {...register('confirm-password', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        /><br/><br/>
+
+
+
+        <FormErrorMessage>
+          {errors.name && errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+        Submit
+      </Button>
+    </form>
+  )
+}
